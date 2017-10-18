91fab0b77f0e3bf51426d8ecf15b4f9983e76d89
Jamesjue <canjian.myself@gmail.com>
2017-10-18 09:13:58 -0400

    add in start demo scripts and gitignore

---
+++ start_demo.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+set -e
+
+function die { echo $1; exit 42; }
+
+# Dependency checks
+# source torch if user indicates it's not activated by default
+if [ -z ${GABRIELPATH+x} ]
+then
+   die "Gabriel Not Found. Please specify environment variable GABRIELPATH to be Gabriel's root directory";
+else
+   echo "User specified Gabriel at ${GABRIELPATH}";
+fi
+
+echo "launching Gabriel at ${GABRIELPATH}"
+cd $GABRIELPATH/server/bin
+./gabriel-control &> /tmp/gabriel-control.log &
+sleep 5
+# NOTE: if the default interface is not eth0 network/util.py change get_ip default interface
+./gabriel-ucomm -s 127.0.0.1:8021 &> /tmp/gabriel-ucomm.log &
+sleep 5
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if pgrep -f "gabriel-ucomm" > /dev/null
+then
+    cd $DIR
+    ./ribloc
+fi
+
+wait
